c0c52aa07db0ad8d60855a15bccfab6ff260000f
aditya-nord <aditya@nordfinance.io>
2023-06-23 21:24:57 +0530

    Vue Library Setup

---
+++ packages/reputex-vue-widget/__tests__/reputex-vue-widget.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const reputexVueWidget = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(reputexVueWidget(), 'Hello from reputexVueWidget');
+console.info('reputexVueWidget tests passed');
---
+++ packages/reputex-vue-widget/lib/components.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils';
+
+import type { JSX } from 'stencil-library';
+
+
+
+
+export const MyButton = /*@__PURE__*/ defineContainer<JSX.MyButton>('my-button', undefined, [
+  'label'
+]);
+
