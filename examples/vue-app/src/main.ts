8472a339e0b5e63af82a84d8a7b5ef0c4b8e9245
aditya-nord <aditya@nordfinance.io>
2023-06-23 21:25:08 +0530

    Example Vue App

---
+++ examples/vue-app/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { ComponentLibrary } from "reputex-vue-widget";
+
+createApp(App).use(ComponentLibrary).mount("#app");
